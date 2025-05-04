a87db63afacd53406ee984560d20f0b0ddd6d785
shubham kumar <xaocofachlys@gmail.com>
2025-05-04 17:16:44 +0530

    logging and exception outline

---
+++ src/logger.py
@@ -0,0 +1,19 @@
+import logging 
+import os
+import sys
+from datetime import datetime
+from src.exception import CustomException
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d')}.log"
+LOG_PATH = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(os.path.dirname(LOG_PATH), exist_ok=True)
+logging.basicConfig(
+    filename=LOG_PATH,
+    format='[%(asctime)s] %(lineno)d %(levelname)s - %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
