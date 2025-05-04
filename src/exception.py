fe2d36849f5e7ef802a9710230725ed802218175
shubham kumar <xaocofachlys@gmail.com>
2025-05-04 17:50:20 +0530

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys 
-
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -22,4 +22,5 @@ if __name__ == "__main__":
     try:
         a = 1/0
     except Exception as e:
+        logging.info("Divide by zero error")
         raise CustomException(e,sys) from e
